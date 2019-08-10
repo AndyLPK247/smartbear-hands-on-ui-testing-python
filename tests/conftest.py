60e8756fc3474c3b3ae1f44222a65d909f5b6025
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-10 13:25:18 -0400

    Added result_uploader fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,13 @@ This module contains shared fixtures.
 
 import json
 import pytest
+import requests
 import selenium.webdriver
 
+
+# ----------------------------------------------------------------------
+# Fixture: Read the CrossBrowserTesting config file
+# ----------------------------------------------------------------------
 
 @pytest.fixture
 def cbt_config(scope='session'):
@@ -29,16 +34,21 @@ def cbt_config(scope='session'):
   return config
 
 
+# ----------------------------------------------------------------------
+# Fixture: Create the WebDriver instance
+# ----------------------------------------------------------------------
+
 @pytest.fixture
-def browser(cbt_config):
+def browser(cbt_config, request):
 
   # Concatenate the URL
-  username = cbt_config['authentication']['username']
+  username = cbt_config['authentication']['username'].replace('@', '%40')
   key = cbt_config['authentication']['key']
   url = f"http://{username}:{key}@hub.crossbrowsertesting.com:80/wd/hub"
 
   # Request a remote browser from CrossBrowserTesting
   caps = cbt_config['webdriver']
+  caps['name'] += ' | ' + request.node.name
   b = selenium.webdriver.Remote(desired_capabilities=caps, command_executor=url)
 
   # Make its calls wait up to 30 seconds for elements to appear
@@ -49,3 +59,38 @@ def browser(cbt_config):
 
   # Quit the WebDriver instance for the cleanup
   b.quit()
+
+
+# ----------------------------------------------------------------------
+# Hook + Fixture: Report the test result to CrossBrowserTesting
+# ----------------------------------------------------------------------
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+  # Expose the test result to the request
+  outcome = yield
+  setattr(item, 'test_result', outcome.get_result())
+
+
+@pytest.fixture
+def result_uploader(cbt_config, browser, request):
+  
+  # Let the test run
+  yield
+
+  # Determine the score
+  score = 'fail' if request.node.test_result.failed else 'pass'
+
+  # Get config data
+  username = cbt_config['authentication']['username']
+  key = cbt_config['authentication']['key']
+
+  # Create the result uploader
+  uploader = requests.Session()
+  uploader.auth = (username, key)
+  
+  # Post the test result
+  uploader.put(
+    f'https://crossbrowsertesting.com/api/v3/selenium/{browser.session_id}',
+    data={'action': 'set_score', 'score': score})
